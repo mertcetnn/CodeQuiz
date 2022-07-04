8d57df0e4130d63cb607b750477c2f1939ffad4b
MertCetin <mert.cetnn@gmail.com>
2022-07-04 00:08:57 -0400

    questions arrays are added

---
+++ script.js
@@ -0,0 +1,36 @@
+var question1 = ["Commonly used data types DO NoT Include:"]
+var answers =["String","Boolens","Alerts","Numbers"] 
+var correctA=["Boolens"]
+var correctA=true
+if (correctA === true){
+    console.log("ssss")
+} else {false}
+ answers [0,2,3]=false
+if (answers[0,2,3] === false)
+
+
+
+
+
+
+
+
+
+var question2 = ["The condition in an if / else statement is enclosed with ______."]
+
+
+
+
+
+var question3 = ["Arrays in JavaScript can be used to store _______."]
+
+
+
+
+var question4 = ["String values must be enclosed within______when being assigned to variables."]
+
+
+
+
+var question5 = ["A very useful tool used during development and debugging for printing content to the debugger is:"]
+
